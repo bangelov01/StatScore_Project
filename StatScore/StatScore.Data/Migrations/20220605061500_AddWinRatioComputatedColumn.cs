252fcf6d8a93ec23d91f28c85c3afcd9548b2909
Bogdan Angelov <bgd.angelov@gmail.com>
2022-06-05 09:16:54 +0300

    Add computated column WinRatio to DB

---
+++ StatScore/StatScore.Data/Migrations/20220605061500_AddWinRatioComputatedColumn.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StatScore.Data.Migrations
+{
+    public partial class AddWinRatioComputatedColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "WinRatio",
+                table: "LeagueStats",
+                type: "float",
+                nullable: false,
+                computedColumnSql: "(CAST([Wins] AS FLOAT) / CAST([Wins] + [Losses] + [Draws] AS FLOAT)) * 100",
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "WinRatio",
+                table: "LeagueStats",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldComputedColumnSql: "(CAST([Wins] AS FLOAT) / CAST([Wins] + [Losses] + [Draws] AS FLOAT)) * 100");
+        }
+    }
+}
